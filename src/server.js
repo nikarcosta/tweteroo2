f2142eec64128578cc22eabf4edabc424b238ceb
nikarcosta <mrc32467098@outlook.com>
2024-01-31 17:27:29 -0300

    feat:add sign up endpoint

--- src/server.js
+++ src/server.js
@@ -3,6 +3,22 @@ import express from "express";
 const server = express();
 server.use(express.json());
 
+let users = [];
+
+server.post("/sign-up", (req, res) => {
+  let userName = req.body.username;
+  let userAvatar = req.body.avatar;
+
+  if (userName === "" || userAvatar === "") {
+    res.send("Preencha todos os campos!");
+  }
+
+  let userInfo = { userName, userAvatar };
+  users.push(userInfo);
+
+  res.send(users);
+});
+
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`Server running at PORT = ${PORT}.`);
