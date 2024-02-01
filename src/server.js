81a2671277c052d4384b8e31542653c55780d1f9
nikarcosta <mrc32467098@outlook.com>
2024-02-01 18:43:34 -0300

    feat: add get tweets by username endpoint

--- src/server.js
+++ src/server.js
@@ -58,6 +58,21 @@ server.get("/tweets", (req, res) => {
   return res.send(usersTweets.reverse().slice(0, 10));
 });
 
+server.get("/tweets/:username", (req, res) => {
+  const username = req.params.username;
+
+  const userTweets = tweets.filter((tweet) => {
+    if (tweet.username === username) {
+      const userInfo = users.find((user) => user.username === tweet.username);
+      console.log(userInfo);
+      return { ...tweet, avatar: userInfo.avatar };
+    }
+  });
+
+  return res.send(userTweets);
+  //WHY ISN'T RETURNING THE USER'S AVATAR?
+});
+
 const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`Server running at PORT = ${PORT}.`);
