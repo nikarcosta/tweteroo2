6974be9987399cc39da4de8428738c71d2c0469d
nikarcosta <mrc32467098@outlook.com>
2024-02-13 09:08:22 -0300

    refactor: remove userSchema, loginSchema, sign-in and sign-up from server.js file and add sign-in sign-up functions to authController.js file

---
+++ src/controllers/authController.js
@@ -0,0 +1,58 @@
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import db from "./config/database.js";
+
+export async function signUp(req, res) {
+  const { email, username, password, avatar } = req.body;
+
+  try {
+    const userExists = await db
+      .collection("users")
+      .findOne({ $or: [{ username }, { email }] });
+    if (userExists) {
+      if (userExists.username === username) {
+        return res.status(404).send("Username already exists!");
+      } else {
+        return res.status(404).send("Email is already in use!");
+      }
+    }
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    await db.collection("users").insertOne({
+      email: email,
+      username: username,
+      password: passwordHash,
+      avatar: avatar,
+    });
+
+    return res.status(201).send("User created successfully!");
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+}
+
+export async function signIn(req, res) {
+  const { username, password } = req.body;
+
+  try {
+    const userExists = await db.collection("users").findOne({ username });
+    if (!userExists) return res.status(404).send("User doesn't exist!");
+
+    if (userExists && bcrypt.compare(password, userExists.password)) {
+      const token = uuid();
+
+      await db
+        .collection("sessions")
+        .insertOne({ userId: userExists._id, token });
+
+      const response = { message: "Welcome back", token };
+
+      return res.status(200).send(response);
+    } else {
+      return res.status(401).send("Invalid username or password!");
+    }
+  } catch (err) {
+    return req.status(500).send(err.message);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,107 +1,15 @@
 import express from "express";
 import cors from "cors";
 import joi from "joi";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
 import db from "./config/database.js";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-const userSchema = joi.object({
-  email: joi.string().email().required(),
-  username: joi.string().required().min(3),
-  password: joi.string().required().min(6),
-  avatar: joi.string().required(),
-});
-
 const tweetSchema = joi.object({
   username: joi.string().required(),
   tweet: joi.string().required(),
-});
-
-const loginSchema = joi.object({
-  username: joi.string().required(),
-  password: joi.string().required(),
-});
-
-server.post("/sign-up", async (req, res) => {
-  const { email, username, password, avatar } = req.body;
-
-  const userInfo = {
-    email,
-    username,
-    password,
-    avatar,
-  };
-
-  const validation = userSchema.validate(userInfo, { abortEarly: false });
-
-  if (validation.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
-  }
-
-  try {
-    const userExists = await db
-      .collection("users")
-      .findOne({ $or: [{ username }, { email }] });
-    if (userExists) {
-      if (userExists.username === userInfo.username) {
-        return res.status(404).send("Username already exists!");
-      } else {
-        return res.status(404).send("Email is already in use!");
-      }
-    }
-
-    const passwordHash = bcrypt.hashSync(password, 10);
-
-    await db.collection("users").insertOne({
-      email: userInfo.email,
-      username: userInfo.username,
-      password: passwordHash,
-      avatar: userInfo.avatar,
-    });
-
-    return res.status(201).send("User created successfully!");
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
-
-server.post("/sign-in", async (req, res) => {
-  const { username, password } = req.body;
-
-  const userLogin = { username, password };
-
-  const validation = loginSchema.validate(userLogin, { abortEarly: false });
-
-  if (validation.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
-    return res.status(422).send(errors);
-  }
-
-  try {
-    const userExists = await db.collection("users").findOne({ username });
-    if (!userExists) return res.status(404).send("User doesn't exist!");
-
-    if (userExists && bcrypt.compare(password, userExists.password)) {
-      const token = uuid();
-
-      await db
-        .collection("sessions")
-        .insertOne({ userId: userExists._id, token });
-
-      const response = { message: "Welcome back", token };
-
-      return res.status(200).send(response);
-    } else {
-      return res.status(401).send("Invalid username or password!");
-    }
-  } catch (err) {
-    return req.status(500).send(err.message);
-  }
 });
 
 server.post("/tweets", async (req, res) => {
