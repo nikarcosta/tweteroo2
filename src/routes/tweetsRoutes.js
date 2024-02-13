4821cf86924593e847dd012b7a41c6afecb2d925
nikarcosta <mrc32467098@outlook.com>
2024-02-13 10:26:43 -0300

    refactor: add authentication and tweets routes to server.js file

--- src/routes/tweetsRoutes.js
+++ src/routes/tweetsRoutes.js
@@ -4,13 +4,18 @@ import {
   getTweetsByUsername,
 } from "../controllers/tweetsController";
 import { Router } from "express";
-import { validateSchema } from "../middlewares/validateSchema";
+import { validateSchema, authMiddleware } from "../middlewares";
 import { tweetSchema } from "../schemas/tweetsSchema";
 
 const tweetsRouter = Router();
 
-tweetsRouter.post("/tweets", validateSchema(tweetSchema), postTweets);
-tweetsRouter.get("/tweets", getTweets);
-tweetsRouter.get("/tweets/:username", getTweetsByUsername);
+tweetsRouter.post(
+  "/tweets",
+  validateSchema(tweetSchema),
+  authMiddleware,
+  postTweets
+);
+tweetsRouter.get("/tweets", authMiddleware, getTweets);
+tweetsRouter.get("/tweets/:username", authMiddleware, getTweetsByUsername);
 
 export default tweetsRouter;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import authRouter from "./routes/authRoutes.js";
+import tweetsRouter from "./routes/tweetsRoutes.js";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use([authRouter, tweetsRouter]);
 
 const PORT = 5000;
 server.listen(PORT, () => {
